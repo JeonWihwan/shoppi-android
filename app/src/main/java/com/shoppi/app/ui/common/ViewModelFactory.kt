200b2a089340b11b833383778ee37188248b96a7
jeonwihwan <dmlghkswq@naver.com>
2023-05-29 22:52:29 +0900

    [섹션12] 장바구니 화면 UI Layer 구현

--- app/src/main/java/com/shoppi/app/ui/common/ViewModelFactory.kt
+++ app/src/main/java/com/shoppi/app/ui/common/ViewModelFactory.kt
@@ -10,6 +10,7 @@ import com.shoppi.app.network.ApiClient
 import com.shoppi.app.repository.*
 import com.shoppi.app.repository.productdetail.ProductDetailRemoteDataSource
 import com.shoppi.app.repository.productdetail.ProductDetailRepository
+import com.shoppi.app.ui.cart.CartViewModel
 import com.shoppi.app.ui.category.CategoryViewModel
 import com.shoppi.app.ui.categorydetail.CategoryDetailViewModel
 import com.shoppi.app.ui.home.HomeViewModel
@@ -35,6 +36,9 @@ class ViewModelFactory(private val context: Context) : ViewModelProvider.Factory
                 val repository = ProductDetailRepository(ProductDetailRemoteDataSource(
                     ServiceLocator.provideApiClient()))
                 ProductDetailViewModel(repository) as T
+            }
+            modelClass.isAssignableFrom(CartViewModel::class.java) -> {
+                CartViewModel() as T
             }
             else -> {
                 throw java.lang.IllegalArgumentException("Failed to create ViewModel: ${modelClass.name}")
